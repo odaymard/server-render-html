b23d61b177219d34765861233738974ce7dc6778
Oday <odaymard88@gmail.com>
2023-05-14 01:12:48 +0200

    readlogs

--- server.js
+++ server.js
@@ -1,13 +1,37 @@
 const express = require('express');
 const requestIp = require('request-ip');
 const app = express();
+const fs = require('fs');
 
 // Middleware to get the client's IP address
 app.use(requestIp.mw());
 
 app.get('/', (req, res) => {
   const clientIp = req.clientIp;
+   const referer = req.headers.referer || '';
+
+  // Create a log entry with the client's IP address and the referer
+  const logEntry = `IP: ${clientIp} | Referer: ${referer}\n`;
+
+  // Append the log entry to reqlog.txt
+  fs.appendFile('reqlog.txt', logEntry, (err) => {
+    if (err) {
+      console.error('Error writing to reqlog.txt:', err);
+    }
+  });
   res.send(`Your IP address is: ${clientIp}`);
+});
+
+app.get('/getlogs', (req, res) => {
+  fs.readFile('reqlog.txt', 'utf8', (err, data) => {
+    if (err) {
+      console.error('Error reading reqlog.txt:', err);
+      res.status(500).send('Error reading logs');
+      return;
+    }
+
+    res.send(data);
+  });
 });
 
 app.listen(3000, () => {
